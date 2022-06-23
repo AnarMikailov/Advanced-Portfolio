b6f7d5c3a64bd4d743e3eedb8193bafdd0b1c1f5
Anar <61512613+AnarMikailov@users.noreply.github.com>
2022-06-23 17:50:01 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      const map = L.map("map").setView([51.505, -0.09], 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
